94a50a3b889ca0dc523558c4e1f2534a126fecc8
shunix <admin@shunix.com>
2016-05-30 02:31:26 +0000

    Avoid fork multiple monitor processes

--- UninstallGuard/src/main/java/com/shunix/uninstallguard/app/GuardApplication.java
+++ UninstallGuard/src/main/java/com/shunix/uninstallguard/app/GuardApplication.java
@@ -1,9 +1,12 @@
 package com.shunix.uninstallguard.app;
 
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import com.shunix.uninstallguard.service.GuardService;
+
+import java.util.List;
 
 /**
  * @author shunix
@@ -13,7 +16,25 @@ public class GuardApplication extends Application {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
-        Intent intent = new Intent(this, GuardService.class);
-        startService(intent);
+        if (!isGuardRunning()) {
+            Intent intent = new Intent(this, GuardService.class);
+            startService(intent);
+        }
+    }
+
+    private boolean isGuardRunning() {
+        boolean result = false;
+        String guardProcessName = getPackageName() + ":guard";
+        ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> processInfos = activityManager.getRunningAppProcesses();
+        if (processInfos != null && processInfos.size() > 0) {
+            for (ActivityManager.RunningAppProcessInfo processInfo : processInfos) {
+                if (processInfo.processName.equalsIgnoreCase(guardProcessName)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
